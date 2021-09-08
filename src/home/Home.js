dc90ec49e9356bec344db136407d142d8e31f31e
komsak007 <lert_zodiac@hotmail.com>
2021-09-08 23:03:53 +0700

    Add featured hot movie and button

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,4 +1,6 @@
+import Featured from "./components/featured/Featured";
 import Navbar from "./components/navbar/Navbar";
+import List from "./components/list/List";
 
 import "./home.scss";
 
@@ -6,11 +8,8 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <img
-        width="100%"
-        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt=""
-      />
+      <Featured />
+      <List />
     </div>
   );
 };
