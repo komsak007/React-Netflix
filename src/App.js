dd1501e699ebe7bf405948c8ae44df183afa4461
komsak007 <lert_zodiac@hotmail.com>
2021-09-12 22:00:24 +0700

    Add Register and login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./app.scss";
 import Home from "./page/home/Home";
+import Login from "./page/login/Login";
+import Register from "./page/register/Register";
 import Watch from "./page/watch/Watch";
 
 function App() {
-  return <Watch />;
+  return <Login />;
 }
 
 export default App;
